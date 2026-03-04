80f6217e222206947b72c9d0584a98fd866c08fe
JoecosB <joecos050628kun@gmail.com>
2026-03-04 22:08:46 +0800

    初始化全局基础设施（common）

---
+++ backend/src/main/java/com/joecos/iam/common/config/WebConfig.java
@@ -0,0 +1,4 @@
+package main.java.com.joecos.iam.common.config;
+
+public class WebConfig {
+}
---
+++ backend/src/main/java/com/joecos/iam/common/exception/BusinessException.java
@@ -0,0 +1,4 @@
+package main.java.com.joecos.iam.common.exception;
+
+public class BusinessException {
+}
