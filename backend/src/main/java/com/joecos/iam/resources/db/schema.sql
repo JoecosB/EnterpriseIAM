c75fc5a646ff1547d05d6555ecfcb701edfc536f
Joecos <130775789+JoecosB@users.noreply.github.com>
2026-03-04 21:51:18 +0800

    数据库设计 (#1)
    * 初始建表脚本
    
    * 修改 sys_permission 中 type 字段为字符串类型
    
    * 初始数据建立脚本

---
+++ backend/src/main/java/com/joecos/iam/resources/db/schema.sql
@@ -0,0 +1,45 @@
+DROP TABLE IF EXISTS sys_user;
+CREATE TABLE sys_user (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    status TINYINT NOT NULL DEFAULT 1,
+    deleted TINYINT NOT NULL DEFAULT 0,
+    create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS sys_role;
+CREATE TABLE sys_role(
+    id SMALLINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    role_code SMALLINT NOT NULL UNIQUE,
+    role_name VARCHAR(50) NOT NULL,
+    description VARCHAR(100)
+);
+
+DROP TABLE IF EXISTS sys_permission;
+CREATE TABLE sys_permission (
+    id SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    permission_name VARCHAR(50) NOT NULL,
+    permission_code VARCHAR(100) NOT NULL UNIQUE,
+    type VARCHAR(50) NOT NULL,
+    parent_id SMALLINT DEFAULT NULL,
+    path VARCHAR(200),
+    component VARCHAR(200),
+    create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS sys_user_role;
+CREATE TABLE sys_user_role (
+    user_id BIGINT NOT NULL,
+    role_id SMALLINT NOT NULL,
+    PRIMARY KEY (user_id, role_id)
+    );
+
+DROP TABLE IF EXISTS sys_role_permission;
+CREATE TABLE sys_role_permission (
+    role_id SMALLINT NOT NULL,
+    permission_id SMALLINT NOT NULL,
+    PRIMARY KEY (role_id, permission_id)
+);
