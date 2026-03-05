bd6d6709b797c2e43073e275ac1cbb216c75213d
JoecosB <joecos050628kun@gmail.com>
2026-03-05 18:09:43 +0800

    在项目入口添加 mapper 扫描器

--- backend/src/main/java/com/joecos/iam/IamApplication.java
+++ backend/src/main/java/com/joecos/iam/IamApplication.java
@@ -1,8 +1,10 @@
 package com.joecos.iam;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.joecos.iam.infrastructure.persistence.mapper")
 @SpringBootApplication
 public class IamApplication {
     public static void main(String[] args) {
