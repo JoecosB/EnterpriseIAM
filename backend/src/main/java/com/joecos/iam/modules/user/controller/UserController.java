6bd1886e830e02937a42855ff2d3e38512485a62
JoecosB <joecos050628kun@gmail.com>
2026-03-10 21:13:30 +0800

    构建 UserController

--- backend/src/main/java/com/joecos/iam/modules/user/controller/UserController.java
+++ backend/src/main/java/com/joecos/iam/modules/user/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.joecos.iam.modules.user.controller;
 
+import com.joecos.iam.modules.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.joecos.iam.infrastructure.persistence.entity.UserEntity;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+    /** 获取用户信息 */
+    @GetMapping("/{id}")
+    public UserEntity getUserById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    /** 获取用户权限 */
+    @GetMapping("/{id}/permissions")
+    public List<String> getUserPermissions(@PathVariable Long id) {
+        return userService.getUserPermissionString(id);
+    }
 }
