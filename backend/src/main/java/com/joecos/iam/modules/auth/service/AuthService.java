4943cd4999e0c7fb349e27eba542d91340050ee2
JoecosB <joecos050628kun@gmail.com>
2026-03-10 20:12:00 +0800

    构建 AuthService

--- backend/src/main/java/com/joecos/iam/modules/auth/service/AuthService.java
+++ backend/src/main/java/com/joecos/iam/modules/auth/service/AuthService.java
@@ -1,4 +1,8 @@
 package com.joecos.iam.modules.auth.service;
 
-public class AuthService {
+import com.joecos.iam.modules.auth.model.AuthResult;
+
+public interface AuthService {
+    Boolean loginByUsername(String username, String password);
+    AuthResult loadUser(Long userId);
 }
