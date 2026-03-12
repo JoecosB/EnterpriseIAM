fbb0b4563a6a4d0e42f607b824628f994772e998
JoecosB <joecos050628kun@gmail.com>
2026-03-12 21:28:05 +0800

    优化 JwtServiceImpl 的 import 语句

--- backend/src/main/java/com/joecos/iam/security/jwt/JwtServiceImpl.java
+++ backend/src/main/java/com/joecos/iam/security/jwt/JwtServiceImpl.java
@@ -1,7 +1,6 @@
 package com.joecos.iam.security.jwt;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
