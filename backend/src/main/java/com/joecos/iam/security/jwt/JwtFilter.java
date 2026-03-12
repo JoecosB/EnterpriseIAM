b272f12c003fa61bc2b197fe2864288dded244e0
JoecosB <joecos050628kun@gmail.com>
2026-03-12 22:32:01 +0800

    构建 JwtFilter

--- backend/src/main/java/com/joecos/iam/security/jwt/JwtFilter.java
+++ backend/src/main/java/com/joecos/iam/security/jwt/JwtFilter.java
@@ -1,4 +1,64 @@
 package com.joecos.iam.security.jwt;
 
-public class JwtFilter {
+import com.joecos.iam.infrastructure.persistence.entity.UserEntity;
+import com.joecos.iam.modules.user.service.UserService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.Authentication;
+
+import java.io.IOException;
+import java.util.Collections;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        // 读取 Authorization Header
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+
+        // 提取并验证 Token
+        String token = authHeader.substring(7);
+        if (!jwtService.isTokenValid(token)) {
+            filterChain.doFilter(request, response);
+        }
+
+        // 加载用户
+        Claims claims = jwtService.extractClaim(token);
+        Long userId = jwtService.extractUserId(claims);
+        UserEntity user = userService.findById(userId);
+
+        // 构造 Authentication
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(
+                        user,
+                        null,
+                        Collections.emptyList()
+                );
+
+        // 构造 Security Context
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(authentication);
+    }
 }
