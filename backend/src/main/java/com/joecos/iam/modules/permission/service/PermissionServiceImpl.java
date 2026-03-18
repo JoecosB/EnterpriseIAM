63a6c7e3c3b99a4565f5d12f4f2a305faaf5d908
JoecosB <joecos050628kun@gmail.com>
2026-03-18 22:40:46 +0800

    在 PermissionService 中添加返回完整系统权限树的方法

--- backend/src/main/java/com/joecos/iam/modules/permission/service/PermissionServiceImpl.java
+++ backend/src/main/java/com/joecos/iam/modules/permission/service/PermissionServiceImpl.java
@@ -3,6 +3,8 @@ package com.joecos.iam.modules.permission.service;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.joecos.iam.infrastructure.persistence.entity.PermissionEntity;
 import com.joecos.iam.infrastructure.persistence.mapper.PermissionMapper;
+import com.joecos.iam.modules.permission.model.PermissionTree;
+import com.joecos.iam.modules.permission.model.PermissionTreeBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ import java.util.List;
 @Service
 public class PermissionServiceImpl implements PermissionService {
     private final PermissionMapper permissionMapper;
+    private final PermissionTreeBuilder builder;
 
 
     /** 根据 ID 查询权限 */
@@ -76,5 +79,13 @@ public class PermissionServiceImpl implements PermissionService {
         }
 
         return codes;
+    }
+
+    /** 返回当前系统权限树 */
+    @Override
+    public List<PermissionTree> getPermissionTree() {
+        List<PermissionEntity> permissions = getAllPermissions();
+
+        return builder.buildPermissionTree(permissions);
     }
 }
