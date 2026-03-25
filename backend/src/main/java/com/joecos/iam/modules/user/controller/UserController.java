2c0562b1df996cdf4f00e9d568f448673d6caf73
JoecosB <joecos050628kun@gmail.com>
2026-03-25 19:55:39 +0800

    修改 Controller 使用 ApiResponse

--- backend/src/main/java/com/joecos/iam/modules/user/controller/UserController.java
+++ backend/src/main/java/com/joecos/iam/modules/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.joecos.iam.modules.user.controller;
 
+import com.joecos.iam.common.api.ApiResponse;
 import com.joecos.iam.modules.user.model.UserDTO;
 import com.joecos.iam.modules.user.model.requests.AssignUserRolesRequest;
 import com.joecos.iam.modules.user.model.requests.CreateUserRequest;
@@ -16,67 +17,69 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
     private final UserService userService;
 
     /** 获取用户权限 */
     @PreAuthorize("hasAuthority('user:list')")
     @GetMapping("/{id}/permissions")
-    public List<String> getUserPermissions(@PathVariable Long id) {
-        return userService.findPermissionCodesById(id);
+    public ApiResponse<List<String>> getUserPermissions(@PathVariable Long id) {
+        return ApiResponse.success(userService.findPermissionCodesById(id));
     }
 
     /** 创建用户 */
     @PreAuthorize("hasAuthority('user:create')")
     @PostMapping
-    public Long createUser(@RequestBody CreateUserRequest request) {
-        return userService.createUser(request);
+    public ApiResponse<Long> createUser(@RequestBody CreateUserRequest request) {
+        return ApiResponse.success(userService.createUser(request));
     }
 
     /** 查询用户列表 */
     @PreAuthorize("hasAuthority('user:list')")
     @GetMapping
-    public List<UserDTO> getUserList() {
-        return userService.getUserList();
+    public ApiResponse<List<UserDTO>> getUserList() {
+        return ApiResponse.success(userService.getUserList());
     }
 
     /** 获取用户信息 */
     @PreAuthorize("hasAuthority('user:list')")
     @GetMapping("/{id}")
-    public UserDTO getUserById(@PathVariable Long id) {
-        return userService.getUserById(id);
+    public ApiResponse<UserDTO> getUserById(@PathVariable Long id) {
+        return ApiResponse.success(userService.getUserById(id));
     }
 
     /** 更新用户信息 */
     @PreAuthorize("hasAuthority('user:update')")
     @PutMapping("/{id}")
-    public void updateUserInfo(@PathVariable Long id,
-                               @RequestBody UpdateUserRequest request
-    ) {
+    public ApiResponse<Void> updateUserInfo(@PathVariable Long id,
+                                            @RequestBody UpdateUserRequest request) {
         userService.updateUserInfo(id, request);
+        return ApiResponse.success(null);
     }
 
     /** 删除用户 */
     @PreAuthorize("hasAuthority('user:delete')")
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id) {
+    public ApiResponse<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
+        return ApiResponse.success(null);
     }
 
     /** 更新用户状态 */
     @PreAuthorize("hasAuthority('user:update')")
     @PatchMapping("/{id}/status")
-    public void updateUserStatus(@PathVariable Long id,
-                                 @RequestBody UpdateUserStatusRequest request
-    ) {
+    public ApiResponse<Void> updateUserStatus(@PathVariable Long id,
+                                              @RequestBody UpdateUserStatusRequest request) {
         userService.updateUserStatus(id, request);
+        return ApiResponse.success(null);
     }
 
     /** 更新用户身份组 */
     @PreAuthorize("hasAuthority('user:assign')")
     @PostMapping("/{id}/roles")
-    public void assignUserRoles(@PathVariable Long id,
-                                @RequestBody AssignUserRolesRequest request
-    ) {
+    public ApiResponse<Void> assignUserRoles(@PathVariable Long id,
+                                             @RequestBody AssignUserRolesRequest request) {
         userService.assignUserRoles(id, request);
+        return ApiResponse.success(null);
     }
 }
