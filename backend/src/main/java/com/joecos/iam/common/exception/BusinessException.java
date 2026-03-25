bf75335ce3c9835ae35366a5764d04a3d4e45aff
JoecosB <joecos050628kun@gmail.com>
2026-03-25 19:06:21 +0800

    搭建 BusinessException

--- backend/src/main/java/com/joecos/iam/common/exception/BusinessException.java
+++ backend/src/main/java/com/joecos/iam/common/exception/BusinessException.java
@@ -1,4 +1,29 @@
 package com.joecos.iam.common.exception;
 
-public class BusinessException {
+
+import com.joecos.iam.common.constant.ErrorCode;
+import lombok.Getter;
+
+public class BusinessException extends RuntimeException {
+
+    @Getter
+    private final Integer code;
+    private final String message;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
